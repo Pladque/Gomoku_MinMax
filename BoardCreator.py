00e20085a72a2a023e3da74a2b57cc503ab45664
Pladque <63751303+Pladque@users.noreply.github.com>
2020-12-11 11:38:33 +0100

    ss

--- BoardCreator.py
+++ BoardCreator.py
@@ -1,8 +1,21 @@
+import pyautogui
+from PIL import Image
+import image_slicer
+import glob
+
+def takeScreenshot ():
+    
+    myScreenshot = pyautogui.screenshot()
+    myScreenshot.save(r"D:\Projects\PythonProjects\PrivateGithub\Tic-Tac-Toe_MinMax\ss.png")
+
+
+
 file_move_from_cpp_r = open("string_index_return.txt", 'r')
 file_moves_data_r = open("calculated_moves.txt", 'r')
 file_board_w = open("board.txt", 'w')
 
 #make ss
+takeScreenshot ()
 #find white and balck
 #create board
 #search board in file_moves_data_r
