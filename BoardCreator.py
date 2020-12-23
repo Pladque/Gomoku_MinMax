422b22ed7679a747aa0aa3d8b8e265bc631c33b2
Pladque <63751303+Pladque@users.noreply.github.com>
2020-12-23 15:13:05 +0100

    cpp done, python work, just make it better

--- BoardCreator.py
+++ BoardCreator.py
@@ -9,78 +9,112 @@ from board_coords import *
 import subprocess
 import os
 from subprocess import Popen, PIPE
+from termcolor import colored
+
+BLACK = (40, 40, 40)
+WHITE = (243, 243, 243)
+AI = BLACK
+OPPONENT = WHITE
 
 # TODO
-# changable if AI is black or opponent is black
-# fix board evaluation in .cpp bc now it consider fe XOOOOX as -4 :)
+# rading from file previously calculated doesnt work sometimes idk why
+# open file once and save it to list, do not ope it every time
+# make loop except of making ss by maniually
+# make use of x and y substractors
+# make auto clicker
 
 
-file_moves_data_r = open("calculated_moves.txt", 'r')
-file_board_w = open("board.txt", 'w')
+def get_curr_board(ss_gap:int) ->str:
+    time.sleep(ss_gap)
+    SS = ImageGrab.grab(bbox=(410,130,963,685))
+    SS.save("D:\Projects\PythonProjects\PrivateGithub\Tic-Tac-Toe_MinMax\ss.png")
 
-#make ss
-time.sleep(0)
-SS = ImageGrab.grab(bbox=(410,130,963,685))
-SS.save("D:\Projects\PythonProjects\PrivateGithub\Tic-Tac-Toe_MinMax\ss.png")
+    #create board
 
-#find white and balck
-#create board
-SS = ImageGrab.grab(bbox=(410,130,963,685)).load()
-board_str = ""
-for y in FIELD_Y:
-    for x in FIELDS_X:
-        if SS[x,y] == (243, 243, 243):
-            board_str += 'O'
-        elif SS[x,y] == (40, 40, 40):
-            board_str += 'X'
-        else:
-            board_str+='0'
+    SS = ImageGrab.grab(bbox=(410,130,963,685)).load()
+    board_str = ""
+    for y in FIELD_Y:
+        for x in FIELDS_X:
+            if SS[x,y] == OPPONENT:
+                board_str += 'O'
+            elif SS[x,y] == AI:
+                board_str += 'X'
+            else:
+                board_str+='0'
+
+    return board_str
 
 #search board in file_moves_data_r
-move = 0
-for line in file_moves_data_r.readlines():
-    if  board_str == line[:line.index(';')].replace(' ', ''):
-        move = int(line[line.index(';')+1:].replace(' ', '').replace('\n', ''))
-        break
-file_moves_data_r.close()
+def find_in_previously_calculated(board_str:str) -> int:
+    file_moves_data_r = open("calculated_moves.txt", 'r')
+    move = 0
+    for line in file_moves_data_r.readlines():
+        if  board_str == line[:line.index(';')].replace(' ', ''):
+            move = int(line[line.index(';')+1:].replace(' ', '').replace('\n', ''))
+            return move
+    file_moves_data_r.close()
 
-#calc good depth
-depth = 5  #temp
+    return -1
 
-# Find good square
-left_x_substr, right_x_substr, left_y_substr, right_y_substr = 0,14,0,14    #temp
+def save_parameters_to_file(board_str:str, depth:int, left_x_substr:int, right_x_substr:int,left_y_substr:int, right_y_substr:int):
+    file_board_w = open("board.txt", 'w')
+    file_board_w.write(board_str)
+    file_board_w.write('\n')
+    file_board_w.write(str(depth))
 
-#save board and depth to file_board_w
-file_board_w.write(board_str)
-file_board_w.write('\n')
-file_board_w.write(str(depth))
+    file_board_w.write('\n')
 
-file_board_w.write('\n')
+    file_board_w.write(str(left_x_substr))
+    file_board_w.write('\n')
+    file_board_w.write(str(right_x_substr))
+    file_board_w.write('\n')
+    file_board_w.write(str(left_y_substr))
+    file_board_w.write('\n')
+    file_board_w.write(str(right_y_substr))
 
-file_board_w.write(str(left_x_substr))
-file_board_w.write('\n')
-file_board_w.write(str(right_x_substr))
-file_board_w.write('\n')
-file_board_w.write(str(left_y_substr))
-file_board_w.write('\n')
-file_board_w.write(str(right_y_substr))
+    file_board_w.close()
 
-file_board_w.close()
-
-#run .exe 
-subprocess.check_call(['D:\Projects\PythonProjects\PrivateGithub\Tic-Tac-Toe_MinMax\MULTITHREAD tictactoe cpp 15x15.exe'])
+def run_cpp():
+    subprocess.check_call(['D:\Projects\PythonProjects\PrivateGithub\Tic-Tac-Toe_MinMax\MULTITHREAD tictactoe cpp 15x15.exe'])
 #o = subprocess.Popen(['cmd','/c',r'D:\Projects\PythonProjects\PrivateGithub\Tic-Tac-Toe_MinMax\EXE\MULTITHREAD tictactoe cpp 15x15.exe'])
 #o.wait()  
 
-#w8 for end
-file_move_from_cpp_r = open("string_index_return.txt", 'r')
-ind = file_move_from_cpp_r.readline()
-ind_x = int(ind) %15
-ind_y = int(int(ind) /15)
-print(ind_x, ind_y)
-file_move_from_cpp_r.close()
+def print_board_from_file(board_str:str):
+    file_move_from_cpp_r = open("string_index_return.txt", 'r')
+    ind = file_move_from_cpp_r.readline()
+    ind = int(ind)
+    ind_x = int(ind) %15
+    ind_y = int(int(ind) /15)
+    print(ind_x, ind_y)
+    file_move_from_cpp_r.close()
+    board_list = list(board_str)
+    board_list[ind_y * 15 + ind_x] = 'X'        
+
+    for y in range(15):
+        for x in range(15):
+            if y*15 + x == ind_y * 15 + ind_x:
+                print(colored(board_list[y*15 + x], 'blue', 'on_white'), end = '')
+            elif board_list[y*15 + x] == 'O':
+                print(colored(board_list[y*15 + x], 'red'), end = '')
+            elif board_list[y*15 + x] == 'X':
+                print(colored(board_list[y*15 + x], 'green'), end = '')
+            elif board_list[y*15 + x] == '0':
+                print(colored(board_list[y*15 + x], 'white'), end = '')
+                
+                
+        print()
 #read file_move_from_cpp_r file
 #convert it to coords
 #click on right spot on screen
+
+if __name__ == '__main__':
+    board_str = get_curr_board(1)
+    move = find_in_previously_calculated(board_str)
+    if move != -1:
+        board_str[move] = AI
+    else:
+        save_parameters_to_file(board_str, 5, 0, 14, 0, 14)
+        run_cpp()
+    print_board_from_file(board_str)
 
 
--- CppFiles/MinMax.cpp
+++ CppFiles/MinMax.cpp
@@ -15,14 +15,15 @@ using namespace std;
 //11. try some optymalization (group ifs,less math etc)
 //18. ask on group if it is easy to reprogram it to GPU
 //19. make bigger data prevoiusly calculated moves
+//20. consider first new move optyions
 
-int ScoreBoard(vector<vector<char>>& board, char& maximaler, char& minimaler);
-int EvalBoard(vector<vector<char>>& board, char& maximaler, char& minimaler);
-void GetAllMoves(vector<vector<char>>& board, vector<vector<bool>>& moves);
-char GetWinner(vector<vector<char>>& board);
-int FindBestMove(vector<vector<char>> board, char turn, int depth, int alpha, int beta, char& maximaler, char& minimaler, int move_x, int move_y);
+int ScoreBoard(char board[15][15], char& maximaler, char& minimaler);
+int EvalBoard(char board[15][15], char& maximaler, char& minimaler);
+void GetAllMoves(char board[15][15], bool moves[15][15]);
+char GetWinner(char board[15][15]);
+int FindBestMove(char board[15][15], char turn, int depth, int alpha, int beta, char& maximaler, char& minimaler, int move_x, int move_y);
 
-map<vector<vector<char>>, int> ALLBOARDS;
+//map<vector<vector<char>>, int> ALLBOARDS;
 
 class MinMax
 {
@@ -42,7 +43,7 @@ MinMax::MinMax(char AI_Char)
 		Opponent_character = 'X';
 }
 
-int FindBestMove(vector<vector<char>> board, char turn, int depth, int alpha, int beta, char& maximaler, char& minimaler,
+int FindBestMove(char board[15][15], char turn, int depth, int alpha, int beta, char& maximaler, char& minimaler,
 	int move_x, int move_y)
 {
 	if (depth == 0)
@@ -55,21 +56,27 @@ int FindBestMove(vector<vector<char>> board, char turn, int depth, int alpha, in
 	{
 		return (winner == maximaler) ? INT_MAX : INT_MIN;
 	}
+	char my_board[15][15];
 
-	board[move_x][move_y] = (turn == minimaler) ? maximaler : minimaler;
+	for (int i = 0; i < 15; i++)
+		for (int j = 0; j < 15; j++)
+			my_board[i][j] = board[i][j];
+
+	my_board[move_x][move_y] = (turn == minimaler) ? maximaler : minimaler;
 
 	//skipping already considered boards:
-	if (ALLBOARDS.find(board) != ALLBOARDS.end())
+	//if (ALLBOARDS.find(board) != ALLBOARDS.end())
 	{
-		return ALLBOARDS[board];
+		//return ALLBOARDS[board];
 	}
 
 	int bestscore = (turn == maximaler) ? INT_MIN : INT_MAX;
 
-	vector<vector<bool>> all_moves;
-	GetAllMoves(board, all_moves);
+	//vector<vector<bool>> all_moves;
+	bool all_moves[15][15];
+	GetAllMoves(my_board, all_moves);
 
-	int eval;
+	int eval; //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 
 	short seven_minus_i;
 	short seven_plus_i;
@@ -90,44 +97,44 @@ int FindBestMove(vector<vector<char>> board, char turn, int depth, int alpha, in
 
 				if (all_moves[seven_minus_j][seven_minus_i])
 				{
-					eval = FindBestMove(board, minimaler, depth - 1, alpha, beta, maximaler, minimaler, seven_minus_j, seven_minus_i);
+					eval = FindBestMove(my_board, minimaler, depth - 1, alpha, beta, maximaler, minimaler, seven_minus_j, seven_minus_i);
 					bestscore = max(eval, bestscore);
 					alpha = max(eval, alpha);
 					if (beta <= alpha)
 					{
-						ALLBOARDS[board] = bestscore;
+						//ALLBOARDS[board] = bestscore;
 						return bestscore;
 					}
 				}if (j != 0 && i != 0)
 				{
 					if (all_moves[seven_plus_j][seven_minus_i])
 					{
-						eval = FindBestMove(board, minimaler, depth - 1, alpha, beta, maximaler, minimaler, seven_plus_j, seven_minus_i);
+						eval = FindBestMove(my_board, minimaler, depth - 1, alpha, beta, maximaler, minimaler, seven_plus_j, seven_minus_i);
 						bestscore = max(eval, bestscore);
 						alpha = max(eval, alpha);
 						if (beta <= alpha)
 						{
-							ALLBOARDS[board] = bestscore;
+							//ALLBOARDS[board] = bestscore;
 							return bestscore;
 						}
 					}if (all_moves[seven_minus_j][seven_plus_i])
 					{
-						eval = FindBestMove(board, minimaler, depth - 1, alpha, beta, maximaler, minimaler, seven_minus_j, seven_plus_i);
+						eval = FindBestMove(my_board, minimaler, depth - 1, alpha, beta, maximaler, minimaler, seven_minus_j, seven_plus_i);
 						bestscore = max(eval, bestscore);
 						alpha = max(eval, alpha);
 						if (beta <= alpha)
 						{
-							ALLBOARDS[board] = bestscore;
+							//ALLBOARDS[board] = bestscore;
 							return bestscore;
 						}
 					}if (all_moves[seven_plus_j][seven_plus_i])
 					{
-						eval = FindBestMove(board, minimaler, depth - 1, alpha, beta, maximaler, minimaler, seven_plus_j, seven_plus_i);
+						eval = FindBestMove(my_board, minimaler, depth - 1, alpha, beta, maximaler, minimaler, seven_plus_j, seven_plus_i);
 						bestscore = max(eval, bestscore);
 						alpha = max(eval, alpha);
 						if (beta <= alpha)
 						{
-							ALLBOARDS[board] = bestscore;
+							//ALLBOARDS[board] = bestscore;
 							return bestscore;
 						}
 					}
@@ -149,12 +156,12 @@ int FindBestMove(vector<vector<char>> board, char turn, int depth, int alpha, in
 
 				if (all_moves[seven_minus_j][seven_minus_i])
 				{
-					eval = FindBestMove(board, maximaler, depth - 1, alpha, beta, maximaler, minimaler, seven_minus_j, seven_minus_i);
+					eval = FindBestMove(my_board, maximaler, depth - 1, alpha, beta, maximaler, minimaler, seven_minus_j, seven_minus_i);
 					bestscore = min(eval, bestscore);
 					beta = min(eval, beta);
 					if (beta <= alpha)
 					{
-						ALLBOARDS[board] = bestscore;
+						//ALLBOARDS[board] = bestscore;
 						return bestscore;
 					}
 				}
@@ -162,32 +169,32 @@ int FindBestMove(vector<vector<char>> board, char turn, int depth, int alpha, in
 				{
 					if (all_moves[seven_plus_j][seven_minus_i])
 					{
-						eval = FindBestMove(board, maximaler, depth - 1, alpha, beta, maximaler, minimaler, seven_plus_j, seven_minus_i);
+						eval = FindBestMove(my_board, maximaler, depth - 1, alpha, beta, maximaler, minimaler, seven_plus_j, seven_minus_i);
 						bestscore = min(eval, bestscore);
 						beta = min(eval, beta);
 						if (beta <= alpha)
 						{
-							ALLBOARDS[board] = bestscore;
+							//ALLBOARDS[board] = bestscore;
 							return bestscore;
 						}
 					}if (all_moves[seven_plus_j][seven_plus_i])
 					{
-						eval = FindBestMove(board, maximaler, depth - 1, alpha, beta, maximaler, minimaler, seven_plus_j, seven_plus_i);
+						eval = FindBestMove(my_board, maximaler, depth - 1, alpha, beta, maximaler, minimaler, seven_plus_j, seven_plus_i);
 						bestscore = min(eval, bestscore);
 						beta = min(eval, beta);
 						if (beta <= alpha)
 						{
-							ALLBOARDS[board] = bestscore;
+							//ALLBOARDS[board] = bestscore;
 							return bestscore;
 						}
 					} if (all_moves[seven_minus_j][seven_plus_i])
 					{
-						eval = FindBestMove(board, maximaler, depth - 1, alpha, beta, maximaler, minimaler, seven_minus_j, seven_plus_i);
+						eval = FindBestMove(my_board, maximaler, depth - 1, alpha, beta, maximaler, minimaler, seven_minus_j, seven_plus_i);
 						bestscore = min(eval, bestscore);
 						beta = min(eval, beta);
 						if (beta <= alpha)
 						{
-							ALLBOARDS[board] = bestscore;
+							//ALLBOARDS[board] = bestscore;
 							return bestscore;
 						}
 					}
@@ -196,11 +203,11 @@ int FindBestMove(vector<vector<char>> board, char turn, int depth, int alpha, in
 		}
 	}
 
-	ALLBOARDS[board] = bestscore;
+	//ALLBOARDS[board] = bestscore;
 	return bestscore;
 }
 
-void FindBestScoreInBoardAndReplaceBestIfBetter(int& BestInd, int& BestScore, int& score, vector<vector<char>>& board, char turn, int depth, int& alpha, int& beta,
+void FindBestScoreInBoardAndReplaceBestIfBetter(int& BestInd, int& BestScore, int& score, char board[15][15], char turn, int depth, int& alpha, int& beta,
 	char maximaler, char minimaler, int move_x, int move_y)
 {
 	score = FindBestMove(board, turn, depth, alpha, beta, maximaler, minimaler, move_x, move_y);
@@ -214,23 +221,24 @@ void FindBestScoreInBoardAndReplaceBestIfBetter(int& BestInd, int& BestScore, in
 int GetBestMove(string& str_board, char turn, int depth, char maximaler, char minimaler, int left_x_subtstractor, int right_x_subtstractor, int left_y_subtstractor, int right_y_subtstractor)    //function that i will call from python
 {
 	MinMax* AI = new MinMax(maximaler);
-	vector<vector<char>> board;
+	char board[15][15];
 
 	//converting str 15x15 board to vectors of vector  of chars
+	int j = -1;
 	for (int i = 0; i < str_board.length(); i++)
 	{
 		if (i % 15 == 0)
 		{
-			vector<char> empty_vec;
-			board.push_back(empty_vec);
+			j++;
 		}
 		if (str_board[i] == '0' || str_board[i] == 'O' || str_board[i] == 'X')
 		{
-			board[(int)i / 15].push_back(str_board[i]);
+			board[j][i % 15] = str_board[i];
 		}
 	}
 
-	vector<vector<bool>> possible_moves;
+	//vector<vector<bool>> possible_moves;
+	bool possible_moves[15][15];
 	GetAllMoves(board, possible_moves);
 
 	int bestScore = INT_MIN;
@@ -259,25 +267,25 @@ int GetBestMove(string& str_board, char turn, int depth, char maximaler, char mi
 			seven_plus_j = 7 + j;
 			if (possible_moves[seven_minus_j][seven_minus_i] && seven_minus_j <= right_x_subtstractor && seven_minus_j >= left_x_subtstractor && seven_minus_i <= right_y_subtstractor && seven_minus_i >= left_y_subtstractor)
 			{
-				threads.push_back(thread(FindBestScoreInBoardAndReplaceBestIfBetter, ref(BestInd), ref(bestScore), ref(score1), ref(board), AI->Opponent_character, depth - 1, ref(alpha), ref(beta), maximaler, minimaler, seven_minus_j, seven_minus_i));
+				threads.push_back(thread(FindBestScoreInBoardAndReplaceBestIfBetter, ref(BestInd), ref(bestScore), ref(score1), board, AI->Opponent_character, depth - 1, ref(alpha), ref(beta), maximaler, minimaler, seven_minus_j, seven_minus_i));
 				//FindBestScoreInBoardAndReplaceBestIfBetter( possible_moves[seven_minus_j][seven_minus_i], BestInd, bestScore, score1, board, AI->Opponent_character, depth - 1, alpha, beta, maximaler, minimaler, seven_minus_j, seven_minus_i);
 			}
 
 			if (possible_moves[seven_plus_j][seven_plus_i] && seven_plus_j <= right_x_subtstractor && seven_plus_j >= left_x_subtstractor && seven_plus_i <= right_y_subtstractor && seven_plus_i >= left_y_subtstractor)
 			{
-				threads.push_back(thread(FindBestScoreInBoardAndReplaceBestIfBetter, ref(BestInd), ref(bestScore), ref(score2), ref(board), AI->Opponent_character, depth - 1, ref(alpha), ref(beta), maximaler, minimaler, seven_plus_j, seven_plus_i));
+				threads.push_back(thread(FindBestScoreInBoardAndReplaceBestIfBetter, ref(BestInd), ref(bestScore), ref(score2), board, AI->Opponent_character, depth - 1, ref(alpha), ref(beta), maximaler, minimaler, seven_plus_j, seven_plus_i));
 				//FindBestScoreInBoardAndReplaceBestIfBetter( possible_moves[seven_plus_j][seven_plus_i], (BestInd), (bestScore), (score2), board, AI->Opponent_character, depth - 1, (alpha), (beta), maximaler, minimaler, seven_plus_j, seven_plus_i);
 			}
 
 			if (possible_moves[seven_plus_j][seven_minus_i] && seven_plus_j <= right_x_subtstractor && seven_plus_j >= left_x_subtstractor && seven_minus_i <= right_y_subtstractor && seven_minus_i >= left_y_subtstractor)
 			{
-				threads.push_back(thread(FindBestScoreInBoardAndReplaceBestIfBetter, ref(BestInd), ref(bestScore), ref(score3), ref(board), AI->Opponent_character, depth - 1, ref(alpha), ref(beta), maximaler, minimaler, seven_plus_j, seven_minus_i));
+				threads.push_back(thread(FindBestScoreInBoardAndReplaceBestIfBetter, ref(BestInd), ref(bestScore), ref(score3), board, AI->Opponent_character, depth - 1, ref(alpha), ref(beta), maximaler, minimaler, seven_plus_j, seven_minus_i));
 				//FindBestScoreInBoardAndReplaceBestIfBetter( possible_moves[seven_plus_j][seven_minus_i], (BestInd), (bestScore), (score3), board, AI->Opponent_character, depth - 1, (alpha), (beta), maximaler, minimaler, seven_plus_j, seven_minus_i);
 			}
 
 			if (possible_moves[seven_minus_j][seven_plus_i] && seven_minus_j <= right_x_subtstractor && seven_minus_j >= left_x_subtstractor && seven_plus_i <= right_y_subtstractor && seven_plus_i >= left_y_subtstractor)
 			{
-				threads.push_back(thread(FindBestScoreInBoardAndReplaceBestIfBetter, ref(BestInd), ref(bestScore), ref(score4), ref(board), AI->Opponent_character, depth - 1, ref(alpha), ref(beta), maximaler, minimaler, seven_minus_j, seven_plus_i));
+				threads.push_back(thread(FindBestScoreInBoardAndReplaceBestIfBetter, ref(BestInd), ref(bestScore), ref(score4), board, AI->Opponent_character, depth - 1, ref(alpha), ref(beta), maximaler, minimaler, seven_minus_j, seven_plus_i));
 				//FindBestScoreInBoardAndReplaceBestIfBetter( possible_moves[seven_minus_j][seven_plus_i], (BestInd), (bestScore), (score4), board, AI->Opponent_character, depth - 1, (alpha), (beta), maximaler, minimaler, seven_minus_j, seven_plus_i);
 			}
 		}
@@ -296,7 +304,7 @@ int GetBestMove(string& str_board, char turn, int depth, char maximaler, char mi
 	return BestInd;
 }
 
-char GetWinner(vector<vector<char>>& board)
+char GetWinner(char board[15][15])
 {
 	int i_minus_sth[4];
 	for (int i = 0; i <= 14; i++)
@@ -375,12 +383,10 @@ char GetWinner(vector<vector<char>>& board)
 	return '0';
 }
 //return 15x15 board filled with bool. If field is true then its valid move
-void GetAllMoves(vector<vector<char>>& board, vector<vector<bool>>& moves)
+void GetAllMoves(char board[15][15], bool moves[15][15])
 {
 	for (int i = 0; i <= 14; i++)
 	{
-		vector<bool> empty_vec;
-		moves.push_back(empty_vec);
 		for (int j = 0; j <= 14; j++)
 		{
 			if (board[i][j] == '0' &&
@@ -388,20 +394,20 @@ void GetAllMoves(vector<vector<char>>& board, vector<vector<bool>>& moves)
 					|| (i <= 13 && j <= 13 && board[i + 1][j + 1] != '0') || (i >= 1 && j >= 1 && board[i - 1][j - 1] != '0')
 					|| (i >= 1 && j <= 13 && board[i - 1][j + 1] != '0') || (i <= 13 && j >= 1 && board[i + 1][j - 1] != '0')
 					|| (j <= 13 && board[i][j + 1] != '0') || (j >= 1 && board[i][j - 1] != '0')))
-				moves[i].push_back(true);
+				moves[i][j] = true;
 			else
-				moves[i].push_back(false);
+				moves[i][j] = false;
 		}
 	}
 }
 
 //Evalueate the board
-int EvalBoard(vector<vector<char>>& board, char& maximaler, char& minimaler)
+int EvalBoard(char board[15][15], char& maximaler, char& minimaler)
 {
 	return ScoreBoard(board, maximaler, minimaler);
 }
 
-int ScoreBoard(vector<vector<char>>& board, char& maximaler, char& minimaler) //AI +, player -
+int ScoreBoard(char board[15][15], char& maximaler, char& minimaler) //AI +, player -
 {
 	int i_minus_sth[4];
 	int score = 0;
@@ -431,7 +437,7 @@ int ScoreBoard(vector<vector<char>>& board, char& maximaler, char& minimaler) //
 								return (board[i][j] == maximaler) ? INT_MAX : INT_MIN;
 						}
 						//check column X0XXX
-						else if ( board[i_minus_sth[2]][j] == '0' && board[i_minus_sth[3]][j] == board[i][j])
+						else if (board[i_minus_sth[2]][j] == '0' && board[i_minus_sth[3]][j] == board[i][j])
 						{
 							score += (board[i][j] == maximaler) ? 2 : -2;
 						}
@@ -496,7 +502,7 @@ int ScoreBoard(vector<vector<char>>& board, char& maximaler, char& minimaler) //
 							if (board[i_minus_sth[1]][j - 2] == board[i_minus_sth[2]][j - 3])
 							{
 								if (board[i_minus_sth[3]][j - 4] == '0' && ((i != 14 && j != 14) && board[i + 1][j + 1] == '0'))
-									return (board[i][j] == maximaler) ? INT_MAX/2 : INT_MIN/2;
+									return (board[i][j] == maximaler) ? INT_MAX / 2 : INT_MIN / 2;
 								if (board[i_minus_sth[3]][j - 4] == '0' || board[i + 1][j + 1] == '0')
 									score += (board[i][j] == maximaler) ? 2 : -2;
 								if (board[i_minus_sth[3]][j - 4] == board[i][j] || board[i + 1][j + 1] == board[i][j])
@@ -619,7 +625,7 @@ int main()
 			return 0;
 		}
 
-		vector<vector<char>> board;
+		char board[15][15];
 		/// TEMP CHECKING EVAL
 			/*
 			// COnvertign str_board to vector board
@@ -640,7 +646,7 @@ int main()
 			cout << "board eval: " << EvalBoard(board, x, o) << endl;
 			*/
 
-		/// FINDING BEST MOVE
+			/// FINDING BEST MOVE
 		const clock_t begin_time = clock();
 		int move = 0;
 		move = GetBestMove(str_board, 'X', depth, 'X', 'O', left_x_subtstractor, right_x_subtstractor, left_y_subtstractor, right_y_subtstractor);
@@ -652,23 +658,23 @@ int main()
 		//vector<vector<char>> board;
 		if (print_board)
 		{
-			board.clear();
 			// COnvertign str_board to vector board
+			int j = -1;
 			for (int i = 0; i < str_board.length(); i++)
 			{
 				if (i % 15 == 0)
 				{
-					vector<char> empty_vec;
-					board.push_back(empty_vec);
+					j++;
 				}
 				if (str_board[i] == '0' || str_board[i] == 'O' || str_board[i] == 'X')
 				{
-					board[(int)i / 15].push_back(str_board[i]);
+					board[j][i % 15] = str_board[i];
 				}
 			}
 
 			/// Schowing changed board
-			vector<vector<bool>> moves;
+			//vector<vector<bool>> moves;
+			bool moves[15][15];
 			GetAllMoves(board, moves);
 			for (int i = 0; i < 15; i++)
 			{
